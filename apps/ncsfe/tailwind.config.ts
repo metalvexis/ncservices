ec2299a26fd3527e925b3f0ca5e96605f3bf0219
James Saballegue <jp.saballegue@gmail.com>
2024-05-06 22:23:31 +0800

    feat: add shadcn

--- apps/ncsfe/tailwind.config.ts
+++ apps/ncsfe/tailwind.config.ts
@@ -1,22 +1,17 @@
 import config from '@ncservices/tailwindconfig'
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const extConfig: Config = {
+const extendedConfig = {
   ...config,
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default extConfig;
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  darkMode: ["class"],
+  prefix: "",
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default extendedConfig
